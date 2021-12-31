ec7af7916bfa077118421808d64e0e7feb45d8b8
benjaminbtrance <benjaminb.tran@gmail.com>
2021-12-30 22:52:21 -0600

    Add project files.

---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+
+        public static implicit operator int(ApplicationDbContext v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
